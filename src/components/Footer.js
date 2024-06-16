888471a9cd181c98fa73262162d2cdc8af07fb5d
ONIxJUAN <Tyson.wilssens@outlook.com>
2024-06-16 20:39:41 +0200

    edited footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,27 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
+      <div>
+        <p>
+          <a href="https://www.linkedin.com/in/tyson-wilssens-94394a2b4/">LinkedIn</a>
+        </p>
+        <p>
+          <a href="https://github.com/ONIxJUAN">GitHub</a>
+        </p>
+      </div>
+      <div>
       <p>
         © Tyson Wilssens 2024
       </p>
+      </div>
+      <div>
       <p>
         email: <a href="mailto:tyson.wilssens@outlook.com">tyson.wilssens@outlook.com</a>
       </p>
       <p>gsm: +32 492 40 85 44</p>
+      </div>
+      
+      
     </footer>
   );
 }
